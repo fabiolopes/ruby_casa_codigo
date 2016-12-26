1bc6847d09c3077f813b8de0462de4a552a6fc20
fabio.bione <fabiolopesbione@hotmail.com>
2016-12-26 07:42:17 -0300

    Adicionando getter e setter para preco

--- loja_virtual_ruby/lib/livro.rb
+++ loja_virtual_ruby/lib/livro.rb
@@ -1,13 +1,22 @@
 # encoding: utf-8
 
 class Livro
-	def initialize(autor, isbn = "1", numero_de_paginas)
+	def initialize(autor, isbn = "1", numero_de_paginas, preco)
 		@autor = autor
 		@isbn = isbn
 		@numero_de_paginas = numero_de_paginas
+		@preco = preco
 	end
 
 	def to_s
-		"Autor: #{@autor}; ISBN: #{@isbn}; N. páginas: #{@numero_de_paginas}"
+		"Autor: #{@autor}; ISBN: #{@isbn}; N. páginas: #{@numero_de_paginas}; Preço: #{@preco}"
+	end
+
+	def preco
+		@preco
+	end
+
+	def preco=(preco)
+		@preco = preco
 	end
 end

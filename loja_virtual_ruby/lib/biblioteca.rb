15ffeecb20514ff06a39b8357f696ebf4ed0be6c
Fabio Bione <fabio.bione@accenture.com>
2017-01-19 15:31:48 -0300

    Manipulação arquivo yaml  pg 96

--- loja_virtual_ruby/lib/biblioteca.rb
+++ loja_virtual_ruby/lib/biblioteca.rb
@@ -3,6 +3,7 @@ class Biblioteca
 
 	def initialize
 		@livros = {}
+		@banco_de_arquivos = BancoDeArquivos.new
 		adiciona Livro.new "Testes", "Zé Maria", '1234', 112, 23, :teste
 		adiciona Livro.new "Design", "Lula", '4321', 200, 55, :design
 		adiciona Livro.new "Testes", "Ana Julia", '0090', 512, 53, :teste
@@ -11,17 +12,23 @@ class Biblioteca
 	def adiciona(livro)
 		@livros[livro.categoria] ||= []
 		@livros[livro.categoria] << livro
+		@banco_de_arquivos.salva livro
 	end
 
 	def livros
 		@livros.values.flatten
 	end
 
-	def livros_por_categoria(categoria, bloco_com_p, bloco_com_puts)
+	def livros_por_categoria(categoria)
 		@livros[categoria].each do |livro|
-			bloco_com_p.call livro
-			bloco_com_puts.call livro
+			yeld livro if block_given?
 		end
+	end
+
+	private
+	def salva(livro)
+		@banco_de_arquivos.salva = livro
+		yeld
 	end
 
 	
